0b2c5dc52cad3e5912de001f536260dbb0fcd9ef
sudarshanrajan <sudarshan.rajan.93@gmail.com>
2025-05-15 01:58:20 +0530

    updated host as per railway requirement

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 echo "Starting FastAPI..."
-uvicorn app.data_server:app --port 8000 &
+uvicorn app.data_server:app --host 0.0.0.0 --port 8000 &
 
 echo "Starting Streamlit..."
 streamlit run app/web_app.py \
     --server.port 8501 \
+    --server.address 0.0.0.0 \
     --server.baseUrlPath app &
 
 echo "Starting Nginx..."
